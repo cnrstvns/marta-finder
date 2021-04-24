51f3b3c9a2083b5c5b270edd9cce54ca5b588077
Connor Stevens <conmail1224@gmail.com>
2021-04-24 13:43:55 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+import requests
+import discord
+import boto3
+import os
+
+# Load ENV Variables
+load_dotenv()
+
+
+# Connect to APIs
+client = discord.Client()
+rekog = boto3.client(
+    "rekognition",
+    aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID"), 
+    aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name = os.getenv("AWS_REGION")
+)
+
+# Handle ready event
+@client.event
+async def on_ready():
+    print('Connected to Discord')
+
+# Handle message event
+@client.event
+async def on_message(message):
+    if message.attachments:
+        for attachment in message.attachments:
+            try:
+                image = requests.get(attachment.url)
+                response = rekog.detect_labels(Image = {"Bytes": image.content })
+                if 'Cat' in [item['Name'] for item in response['Labels']]:
+                    await message.delete()
+                    await message.channel.send('Detected a cat')
+            except Exception as e:
+                pass
+
+client.run(os.getenv('TOKEN'))
---
+++ readme.md
@@ -0,0 +1,18 @@
+# marta-finder
+
+marta-finder is a Discord Bot application tasked to delete images of cats from a Discord server.
+
+## Why
+My friend alex has a cat named Marta. We give him a hard time for sending too many pictures of her, so I made this and now we have it under control.
+
+
+## Dependencies
+- discord.py
+- requests
+- boto3
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
